67a23d35aa78510a22495a4b248e8db8183c8cf4
1875167603 <1875167603@qq.com>
2017-12-18 14:16:07 +0800

    edit

--- SerialListener/Listener.cs
+++ SerialListener/Listener.cs
@@ -115,7 +115,7 @@ namespace SerialListener
                         sendReq(rxbytearray);
                         text.Invoke((MethodInvoker)delegate {
                             // Running on the UI thread
-                            text.Text += "上位机指令: "+byteToString(rxbytearray);
+                            text.Text += "下位机回复: " + byteToString(rxbytearray);
                         });
                     }
 
@@ -151,7 +151,7 @@ namespace SerialListener
                         sendRes(rxbytearray);
                         text.Invoke((MethodInvoker)delegate {
                             // Running on the UI thread
-                            text.Text+= "下位机回复:" + byteToString(rxbytearray);
+                            text.Text+= "上位机指令:" + byteToString(rxbytearray);
                         });
                     }
 
